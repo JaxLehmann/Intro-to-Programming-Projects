10095fb9a457789d22df553a866b5999aa70ecbb
Jax Lehmann <jaxlehmann@gmail.com>
2025-02-14 17:46:39 -0500

    Add files via upload

---
+++ ZombieKiller.java
@@ -0,0 +1,43 @@
+
+public class ZombieKiller {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to our zombie killer program!");
+		
+		// list of whatever you want is an array!
+		String backpack [] = {"Shotgun", "Assault Rifle", "Sniper"};
+		String zombies [] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+		
+		System.out.println("Backpack items!");
+		System.out.println(backpack[0]);
+		System.out.println(backpack[1]);
+		System.out.println(backpack[2]);
+		
+		System.out.println("These are the zombies!");
+		System.out.println(zombies[0]);
+		System.out.println(zombies[1]);
+		System.out.println(zombies[2]);
+		
+		String seasons [] = {"Summer", "Winter", "Spring", "Fall"};
+		System.out.println("If you can snowboard out, it is probably the " + seasons[1] + ".");
+		
+		System.out.println(backpack[2]);
+		System.out.println(backpack[2]);
+		System.out.println(backpack[2]);
+		System.out.println(backpack[0]);
+		
+		int numbersZombiesHate[] = {4, 90, 70, 123, 1, 2};
+		
+		System.out.println(numbersZombiesHate[2]);
+		
+		// making an array is easy! set a string, name it, set those indexes!
+		
+		String peripherals [] = {"mouse", "keyboard", "mousepad", "monitor"};
+		System.out.println(peripherals[3]);
+		
+		int areacode [] = {551, 201, 562};
+		System.out.println("Mona's areacode is " + areacode[2]);
+
+	}
+
+}
