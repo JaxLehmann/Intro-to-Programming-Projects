9f38b6d7c09d625983d7ab29e4ff3f9d467e299a
Jax Lehmann <jaxlehmann@gmail.com>
2025-02-27 08:19:47 -0500

    Add files via upload

---
+++ DoubleLongFloat.java
@@ -0,0 +1,21 @@
+
+public class DoubleLongFloat {
+	public static void main (String[] args) {
+		int a = 999999;
+		// long integer is called a long
+		long b = 999999999999L;
+		// need to add a L at the end of a long for it to work
+		// what if we have a decimal??
+		// use a float!
+		float c = 2.5f;
+		// floats are denoted at the end by an f, capital or lower case!
+		// floats also have a red underline if too long, so we use doubles instead!
+		double d = 2.5555555534343;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
