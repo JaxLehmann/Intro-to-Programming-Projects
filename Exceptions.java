7bf936ff019fed3da51395406476d0fe407ae7ae
Jax Lehmann <jaxlehmann@gmail.com>
2025-03-02 17:03:35 -0500

    Add files via upload

---
+++ Exceptions.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Exceptions {
+
+	public static void main(String[] args) {
+		// Exception is an event which occurs during the execution
+		// of a program that disrupts the flow of the program's instructions
+		// Let's make some exceptions!
+
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Book");
+		System.out.println(list.get(0));
+
+	}
+
+}
