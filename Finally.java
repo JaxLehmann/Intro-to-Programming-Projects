4a7e3803f89cb82a7da1c7360662c869c196e311
Jax Lehmann <jaxlehmann@gmail.com>
2025-03-06 19:48:17 -0500

    Add files via upload

---
+++ Finally.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Finally {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		try {
+			System.out.println(scan.next());
+		} catch(Exception e) {
+			System.out.println(e);
+		} finally {
+			scan.close();
+		}
+
+	}
+
+}
