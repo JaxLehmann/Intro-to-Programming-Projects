1d394fe27e274cc57ff38f476a53c63ebead61f8
Jax Lehmann <jaxlehmann@gmail.com>
2025-03-12 03:07:11 -0400

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,18 @@
+
+public class Example1 {
+	
+	private String flavor;
+	
+	public void setFlavor(String newFlavor) {
+		flavor = newFlavor;
+	}
+	
+	public String getFlavor() {
+		return flavor;
+	}
+	
+	public void openBag() {
+		System.out.println("Bag is opened");
+	}
+	
+}
---
+++ Example2.java
@@ -0,0 +1,9 @@
+
+public class Example2 {
+
+		public static void main (String[] args) {
+			Example1 e1 = new Example1();
+			e1.setFlavor("Beef");
+			System.out.println(e1.getFlavor());
+		}
+}
