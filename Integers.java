9aaa9c5706fa5d2c4d598752574cfb1027e3f15d
Jax Lehmann <jaxlehmann@gmail.com>
2025-03-16 19:54:18 -0400

    Add files via upload

---
+++ Integers.java
@@ -0,0 +1,14 @@
+
+public class Integers {
+	public static void main(String [] args) {
+		int a = 5;
+		int b = 2;
+		int c = 0;
+		int d = 99999999;
+		int e = -700;
+		System.out.println(a + b);
+		System.out.println(a + d);
+		System.out.println("The " + "dog");
+		System.out.println(a / b);
+	}
+}
