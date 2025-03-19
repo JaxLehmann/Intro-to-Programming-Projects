c53e4833ee19b420314fe4e70c7087f7d44ca90e
Jax Lehmann <jaxlehmann@gmail.com>
2025-03-19 08:56:41 -0400

    Add files via upload

---
+++ MathIsCool.java
@@ -0,0 +1,29 @@
+
+public class MathIsCool {
+
+	public static void main(String[] args) {
+		
+		// System.out.println(Math.class); variable that says what a class is
+		// System.out.println(Math.E); long double value
+		// Math.pie    is pi
+		// Math.abs   shows absolute value
+		// Math.acos  shows arc cosine value
+		// Math.asin  shows arc sin value
+		// Math.atan  shows arc tan value
+		// Math.atan2 angle of an x y point
+		// Math.cbrt(n) shows cube root
+		// Math.ceil() shows number rounded up
+		// Math.copySign(n, -n2) takes sign of the second number
+		// Math.cos() takes 0.1-1.0 cos value
+		// Math.decrementExact() decrements value
+		// Math.exp() takes exponent of a value
+		// Math.expm1 takes exponent formula
+		// Math.floor() takes floor of a value
+		// Math.floorDiv(n, n2) takes floor of dividing n1/n2
+		// Math.floorMod(n, n2) takes floor of the mod of a number divided by number
+		// Math.getExponent() returns unbiased exponent
+		// Math.hypot(x,y) returns hypotenuse of 2 values
+		// Math.toRadians() takes degrees to radians
+	}
+
+}
