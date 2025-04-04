43985f0dae391b58914e57c272fc3eff0114bdc6
Jax Lehmann <jaxlehmann@gmail.com>
2025-04-04 01:14:38 -0400

    Add files via upload

---
+++ LearnToThrow.java
@@ -0,0 +1,13 @@
+import java.io.BufferedWriter;
+
+public class LearnToThrow {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void write() {
+		BufferedWriter w = new BufferedWriternew (new FileWriter("name.txt()));"
+	}
+
+}
