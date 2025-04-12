48e36159c001031cfbaf1465252afa71b2230d0e
Jax Lehmann <jaxlehmann@gmail.com>
2025-04-12 11:54:26 -0400

    Add files via upload

---
+++ JavaTutorial.java
@@ -0,0 +1,33 @@
+
+public class JavaTutorial {
+	public static void main(String[] args) {
+		System.out.print("A");
+		System.out.print("B");
+		System.out.print("C");
+		System.out.print("D");
+		System.out.print("E");
+
+		// These are how to do comments
+		// Comments are illustrated by two forward slashes, these do not impact the code
+		
+		
+		/* This is a comment on how to list. End it by doing star slash, like this! */
+		
+		// Below is the next area, escape sequences!
+		System.out.println("Dog");
+		System.out.print("The \"fish\" was blue");
+		// the \ is used as an escape sequence. 1 \ starts it and another \ finishes!
+		// \ is an escape character to "escape from the normal rules"
+		System.out.println("The \"Dog\" is brown");
+		System.out.println("Martin Luther King once stated, \"I have a dream!\"");
+		System.out.println("These are double quotes \"\"");
+		System.out.println("This is a backslash \\");
+		
+		System.out.println("What do you mean \"Stinky?\"");
+		System.out.println("This is a tab \t");
+		System.out.println("This \n is \n on \n multiple lines");
+		System.out.println("You are a \n Freak!");
+		
+		
+	}
+}
