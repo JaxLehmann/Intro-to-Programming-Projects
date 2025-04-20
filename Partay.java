406f3387c996519b0788717c7a14f1eeb90531f6
Jax Lehmann <jaxlehmann@gmail.com>
2025-04-20 17:36:27 -0400

    Add files via upload

---
+++ Partay.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Partay {
+	public static void main(String []args) {
+		//using a scanner
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter Pokemon 1:");
+		String pokemon1 = scan.nextLine();
+		System.out.println("Pokemon 1 is "+ pokemon1);
+		
+		System.out.println("Enter Pokemon 2:");
+		String pokemon2 = scan.nextLine();
+		System.out.println("Pokemon 2 is "+ pokemon2);
+		
+		System.out.println("Enter Pokemon 3:");
+		String pokemon3 = scan.nextLine();
+		System.out.println("Pokemon 3 is "+ pokemon3);
+		
+		System.out.println("Enter Pokemon 4:");
+		String pokemon4 = scan.nextLine();
+		System.out.println("Pokemon 4 is "+ pokemon4);
+		
+		System.out.println("Enter Pokemon 5:");
+		String pokemon5 = scan.nextLine();
+		System.out.println("Pokemon 5 is "+ pokemon5);
+	}
+
+}
