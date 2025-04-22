c9d8bea7dc3d544157f45443a3d572a0838c5bc2
Jax Lehmann <jaxlehmann@gmail.com>
2025-04-21 21:20:52 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,8 +2,31 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Car c = new Car();
-
+		Pen p = new Pen();
+		
+		System.out.println(p.color);
+		System.out.println(p.point);
+		System.out.println(p.type);
+		
+		System.out.println(p.clicked);
+		
+		p.click();
+		System.out.println(p.clicked);
+		
+		Headphones h = new Headphones();
+		System.out.println(h.color);
+		System.out.println(h.controls);
+		System.out.println(h.charge);
+		
+		System.out.println(h.power);
+		h.powerOn();
+		System.out.println(h.power);
+		
+		System.out.println(h.volume);
+		h.volumeUp();
+		h.volumeUp();
+		h.volumeUp();
+		h.volumeUp();
+		System.out.println(h.volume);
 	}
-
 }
