ae4a4a2e6d957e4df182cb9f7b6d513c372d59d8
Jax Lehmann <jaxlehmann@gmail.com>
2025-04-30 13:02:32 -0400

    Add files via upload

---
+++ StringsTutorial.java
@@ -0,0 +1,12 @@
+public class StringsTutorial {
+	public static void main (String[] args) {
+		System.out.println("Madlibs!");
+		String username = "magicPony420";
+		//anything in double quotes is a string
+				
+		String adjective = "flamboyant";
+		String verb = "shat";
+		String noun = "dog";
+		System.out.println("My " + adjective + " sister accidentally " + verb + " all over her new " + noun + ".");
+	}
+}
