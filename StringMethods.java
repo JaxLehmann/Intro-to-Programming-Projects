f7f39ffc5e7488c88386063d3b364aea5d52ccac
Jax Lehmann <jaxlehmann@gmail.com>
2025-05-10 23:16:10 -0400

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,19 @@
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		String name = "billy bob joe";
+		//method is a fancy name for code that does a task
+		
+		System.out.println("Name: " + name);
+		System.out.println("Uppercase: " + name.toUpperCase());
+		System.out.println("Lowercase: " + name.toLowerCase());
+		System.out.println("First char " + name.charAt(0));
+		System.out.println("Length: " + name.length());
+		System.out.println("Last char " + name.charAt(12));
+		System.out.println("Substring " + name.substring(10,13));
+		
+		// parantheses with no values means no parameters
+	}
+
+}
