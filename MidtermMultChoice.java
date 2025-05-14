e292d3139340017ff74b7a79c31e9b08f8c5bf14
Jax Lehmann <jaxlehmann@gmail.com>
2025-05-13 23:09:14 -0400

    Add files via upload

---
+++ MidtermMultChoice.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class MidtermMultChoice {
+
+	public static void main(String[] args) {
+		
+		// 1 What keyword is used to call the superclass constructor?
+		// B: Super
+		
+		// 2 What is the main advantage of inheritance in java?
+		// A: Allows code reusability
+		
+		// 3 What is method overriding?
+		// B: Redefining a superclass method in a subclass with the same signature
+		
+		// 4 Which keyword prevents a method from being overwritten?
+		// A: Final
+		
+		// 5 What happens when a class implements an interface but does not
+		// provide method definitions?
+		// A: Compiler error
+		
+		// 6 How many interfaces can a java class implement?
+		// B: More than one
+		
+		// 7 Which of the following messages passed to the string str
+		// could throw a StringIndexOutofBoundsException?
+		// D: str.charAt(2);
+		
+		// 8 If int[] x = new int [15]; and the statement x[-1]
+		// = 0; is executed, which of the following exceptions is thrown?
+		// D: ArrayIndexOutOfBoundsException
+		
+		// 9 Which of the following lists of instance data are accessible in A3?
+		// D: X, Z, A, Q
+		
+		// 10 Which of the following is true with respect to A1, A2, A3?
+		// D: A3 is a subclass of A2 and A2 is a subclass of A1
+		
+		// 11 Which of the following is true regarding the use of instance data
+		// y of class A1?
+		// D: It is only accessible in A1
+		
+		// 12. Which of the following lists of instance data are accessible in class A2?
+		// D: x, z, a , b
+		
+		// Array. An array store 9, 4, 12, 2, 6, 8, 18, use values to answer questions
+		
+		// 13. How many passes will it take in all for insertion sort to sort the array?
+		// C: 6
+		
+		// 14. Which of the lists of numbers show the array after pass 1?
+		// D: 4 9 12 2 6 8 18
+		
+		// 15. In order to implement Comparable in a class, what method(s) must be
+		// defined in that class?
+		// C: compareTo
+		
+		// 16. Which searching algorithm has better performance?
+		// B: Binary Search
+		
+		// 17. What will be the output of the following code? don't get
+		// A: 10
+		
+		// 18. What will be the output? don't get
+		// B: Child class
+		
+		// 19: Output don't get
+		// B: Class child
+		
+		// 20: What is the correct way to handle exceptions in java
+		// B: Using try-catch blocks
+		
+		// 21: What is output?
+		// C: Cannot divide by zero + Execution continues.
+		
+		// 22: What happens if exception is thrown but not caught?
+		// A: This will not execute
+		
+		// 23: What keyword is used to declare a custom exception in Java?
+		// B: Extends exception
+		
+		// 24: Given following code answer questions
+		// If an ArithmeticException arises in the try statement in m3?
+		// A) it is caught in m3
+		
+		// 25: If a NullPointerException arises in the try statement in m1?
+		// C) it is not caught leading to the program terminating
+		
+		// 26: If a NullPointerException arises in the try statement in m3?
+		// E) it is caught in m2
+	
+	}
+
+}
